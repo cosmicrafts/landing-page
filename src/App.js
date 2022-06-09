dc785b5eb2f38c10db1ead29aeabdfa0fbc8a1b0
MoisesLopezp <moicano324@hotmail.com>
2022-06-08 21:02:55 -0500

    Avances de seccion nft anvil

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './sections/header/header';
 import Main from './sections/main/main';
+import NFTAnvil from './sections/nftanvil/nftanvil';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <NFTAnvil/>
     </>
   );
 }
