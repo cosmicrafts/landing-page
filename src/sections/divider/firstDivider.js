5c15ad90de768b6393ba011d7ca4c6a01317190b
pk34 <pk34@live.com.mx>
2022-06-15 13:43:00 -0500

    Integrate Stoll's work

---
+++ src/sections/divider/firstDivider.js
@@ -0,0 +1,22 @@
+/// Stoll
+
+/// React
+import React from "react";
+
+import "../../css/dividerOne.css";
+
+export default function FirstDivider(props){
+
+    return (
+        <>
+            <div className="v18_57">
+                <div className="v18_66"></div>
+                <div className="v18_59"></div>
+                <span className="v18_60">GET ON THE SHIP!</span>
+                <div className="v18_65"></div>
+                <div className="v18_67"></div>
+                <div className="v18_63"></div>
+            </div>
+        </>
+    );
+}
