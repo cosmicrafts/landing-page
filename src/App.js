357bb78e337649f066ebb63da04bba66d7a831d9
pk34 <pk34@live.com.mx>
2022-06-21 18:47:23 -0500

    Improve load times 2.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React, { Suspense } from 'react';
 
-//import About from './sections/about/about';
-//import DividerThree from './sections/divider/dividerThree';
-//import DividerTwo from './sections/divider/dividerTwo';
-//import FirstDivider from './sections/divider/firstDivider';
-//import Footer from './sections/footer/footer';
-//import Fundrising from './sections/fundrising/fundrising';
-//import Future from './sections/future/future';
-//import Header from './sections/header/header';
-//import Main from './sections/main/main';
-//import NFTAnvil from './sections/nftanvil/nftanvil';
-//import Roadmap from './sections/roadmap/roadmap';
-//import Team from './sections/team/team';
-
 import "./css/font.css";
 import "./css/html.css";
 
