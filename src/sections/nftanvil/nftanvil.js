2a74208ae49d2a0199523662d194553e580a0dee
pk34 <pk34@live.com.mx>
2022-06-21 18:46:43 -0500

    Improve load times 2

--- src/sections/nftanvil/nftanvil.js
+++ src/sections/nftanvil/nftanvil.js
@@ -12,13 +12,13 @@ export default function NFTAnvil()
         <Container className="BackGround" fluid style={{backgroundImage: `url(${BackGround})`}}>
             <Row className="justify-content-md-center text-center align-items-center">
                 <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
-                    <Image className="ships" src={Hammer}/>
+                    <Image className="ships" src={Hammer} alt="left ship"/>
                 </Col>
                 <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
-                    <Image id="nftanvil_icon" src={NFTAnvilLogo}/>
+                    <Image id="nftanvil_icon" src={NFTAnvilLogo} alt="NFTAnvil logo"/>
                 </Col>
                 <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
-                    <Image className="ships" src={Goblin}/>
+                    <Image className="ships" src={Goblin} alt="right ship"/>
                 </Col>
             </Row>
             <Row className="justify-content-md-center">
